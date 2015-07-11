6a985e74a751ef4c8c7591b9a22e37717efd081e
Sqbika <sqbika@gmail.com>
2015-07-11 18:04:29 +0200

    Create TileRock.cs

---
+++ YW2DSG/YW2DSG/Tiles/TileRock.cs
@@ -0,0 +1,23 @@
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace YW2DSG.Tiles
+{
+class TileRock : GenericTile
+{
+public TileRock(Vector2 position)
+{
+ShouldGravity = false;
+Position = position;
+Name = "Rock";
+TileID = 2;
+TextureName = TextureManager.GetTextureFromID(TileID);
+ItemDroppedOnRemove = Items.ItemDirt();
+Hardness = 4;
+RequiredTool = 1;
+RequiredToolLevel = 2;
+}
+}
+}
